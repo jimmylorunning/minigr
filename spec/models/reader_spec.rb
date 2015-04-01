373e4fa8ad151933cbb4b6d1db7d22bc4c5150bc
Jimmy <jimmylorunning@gmail.com>
2015-04-01 18:53:10 -0400

    Refactor reader_spec so its more modular

--- spec/models/reader_spec.rb
+++ spec/models/reader_spec.rb
@@ -1,26 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe Reader, type: :model do
+  before(:all) do
+    @reader = FactoryGirl.create(:reader)
+  end
+
   it { should have_many(:copies) }
   it { should have_many(:shelves) }
   it { should have_many(:categories) }
 
   context "reader creation" do
-    before(:all) do
-      @reader = FactoryGirl.create(:reader)
-      @reader2 = FactoryGirl.create(:reader)
-      @reader.following.push(@reader2)
-    end
-
     subject { @reader }
     it { should respond_to(:email) }
     it { should respond_to(:password) }
     it { should be_valid }
+  end
+
+  context "follow relationships" do
+    before(:all) do
+      @reader2 = FactoryGirl.create(:reader)
+      @reader.following.push(@reader2)
+    end
+    
+    subject { @reader }
     it { should respond_to(:followers) }
     it { should respond_to(:following) }
     its(:following) { should eq([@reader2]) }
-    it {
-      expect(@reader2.followers).to eq([@reader]) 
-    }
+    context "for the followed reader" do
+      subject { @reader2 }
+      its(:followers) { should eq([@reader]) }
+    end
   end
 end
