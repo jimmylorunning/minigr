623ec116740e7c5ac8113816b43b86a6696c8e7a
jimmylorunning <jimmylorunning@gmail.com>
2015-04-10 00:03:59 -0400

    A status has many likes

--- app/models/status.rb
+++ app/models/status.rb
@@ -1,3 +1,4 @@
 class Status < ActiveRecord::Base
   belongs_to :reader
+  has_many :likes, as: :likeable
 end
--- spec/models/status_spec.rb
+++ spec/models/status_spec.rb
@@ -6,12 +6,18 @@ RSpec.describe Status, type: :model do
   context "associates" do
     before(:all) do
       @reader = FactoryGirl.create(:reader)
-      @reader.statuses.create(message: "hello world")
+      @status = @reader.statuses.create(message: "hello world")
+      @other = FactoryGirl.create(:reader)
+      @other.likes.create(likeable: @status)
     end
 
     it "with a reader" do
       @reader.statuses.count.should eq(1)
       @reader.statuses.first.message.should eq("hello world")
+    end
+    it "with a like" do
+      @status.likes.count.should eq(1)
+      @status.likes.last.reader.should eq(@other)
     end
   end
 end
